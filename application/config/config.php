17bd063129db7ea79cba5a139c2ac9e98d085e58
Juan Bances <jcbances6@unp.edu.pe>
2021-10-03 14:06:40 -0500

    make it better

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ date_default_timezone_set('America/Lima');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/restaurante/';
+$config['base_url'] = 'https://jb-restaurante.herokuapp.com/';
 define ( "assets", $config ['base_url'] . "assets" ); 
 
 /*
